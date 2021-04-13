1392d68605fc6deaf238985870c95425b140a413
Syaifuddin Zuhri <mochammadsyaifuddinzuhri@gmail.com>
2021-04-13 14:46:09 +0700

    make migration mahasiswa_matakuliah table

---
+++ database/migrations/2021_04_13_073822_create_mahasiswa_matakuliah_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateMahasiswaMatakuliahTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('mahasiswa_matakuliah', function (Blueprint $table) {
+            $table->id();
+            $table->string('mahasiswa_nim', 20)->nullable();
+            $table->unsignedBigInteger('matakuliah_id')->nullable();
+            $table->string('nilai', 2);
+
+            $table->foreign('mahasiswa_nim')->references('nim')->on('mahasiswa');
+            $table->foreign('matakuliah_id')->references('id')->on('matakuliah');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('mahasiswa_matakuliah');
+    }
+}
