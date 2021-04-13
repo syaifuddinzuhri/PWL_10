66b6c6728d88b918495244835c2ffe88f0d9f884
Syaifuddin Zuhri <mochammadsyaifuddinzuhri@gmail.com>
2021-04-13 10:56:39 +0700

    update view create article

--- resources/views/articles/create.blade.php
+++ resources/views/articles/create.blade.php
@@ -1,28 +1,32 @@
 @extends('layouts.app')
 
 @section('content')
-    <section>
+    <section class="my-5">
         <div class="container">
             <div class="row">
-                <div class="col">
+                <div class="col-md-6 offset-md-3 mb-4">
+                    <a href="{{ route('articles.index') }}" class="btn btn-dark">Back</a>
+                </div>
+                <div class="col-md-6 offset-md-3">
                     <form>
                         <div class="form-group">
                             <label for="title">Title</label>
-                            <input type="text" class="form-control" id="title" name="title" placeholder="Enter title">
+                            <input type="text" class="form-control" id="title" name="title" placeholder="Enter title"
+                                required>
                         </div>
                         <div class="form-group">
                             <label for="content">Content</label>
-                            <textarea class="form-control" id="content" name="content" rows="3"
-                                placeholder="Enter content"></textarea>
+                            <textarea class="form-control" id="content" name="content" rows="3" placeholder="Enter content"
+                                required></textarea>
                         </div>
                         <div class="form-group">
-                            <label class="custom-file-label" for="image">Feature Image</label>
+                            <label for="image">Feature Image</label>
                             <div class="custom-file">
-                                <input type="file" class="custom-file-input" id="image" name="image">
+                                <input type="file" class="custom-file-input" id="image" name="image" required>
                                 <label class="custom-file-label" for="image">Choose file</label>
                             </div>
                         </div>
-                        <button class="btn btn-primary" type="submit"></button>
+                        <button class="btn btn-primary" type="submit">Submit</button>
                     </form>
                 </div>
             </div>
