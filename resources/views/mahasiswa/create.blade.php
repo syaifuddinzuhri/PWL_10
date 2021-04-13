d273438ca4629c0704902aec97fe5e5e04784029
Syaifuddin Zuhri <mochammadsyaifuddinzuhri@gmail.com>
2021-04-13 15:00:59 +0700

    Add feature upload profile photo mahasiswa in add new mahasiswa

---
+++ resources/views/mahasiswa/create.blade.php
@@ -0,0 +1,60 @@
+@extends('mahasiswa.layout')
+@section('content')
+    <div class="container mt-5">
+        <div class="row justify-content-center align-items-center">
+            <div class="card" style="width: 24rem;">
+                <div class="card-header">
+                    Tambah Mahasiswa
+                </div>
+                <div class="card-body">
+                    @if ($errors->any())
+                        <div class="alert alert-danger">
+                            <strong>Whoops!</strong> There were some problems with your i
+                            nput.<br><br>
+                            <ul>
+                                @foreach ($errors->all() as $error)
+                                    <li>{{ $error }}</li>
+                                @endforeach
+                            </ul>
+                        </div>
+                    @endif
+                    <form method="post" action="{{ route('mahasiswa.store') }}" id="myForm" enctype="multipart/form-data">
+                        @csrf
+                        <div class="form-group">
+                            <label for="nim">Nim</label>
+                            <input type="text" name="nim" class="form-control" id="nim" aria-describedby="nim">
+                        </div>
+                        <div class="form-group">
+                            <label for="nama">Nama</label>
+                            <input type="nama" name="nama" class="form-control" id="nama" aria-describedby="nama">
+                        </div>
+                        <div class="form-group">
+                            <label for="kelas">Kelas</label>
+                            <select type="kelas" name="kelas" class="form-control" id="kelas">
+                                <option selected disabled>--- Pilih Kelas ---</option>
+                                @foreach ($kelas as $kls)
+                                    <option value="{{ $kls->id }}">{{ $kls->nama_kelas }}</option>
+                                @endforeach
+                            </select>
+                        </div>
+                        <div class="form-group">
+                            <label for="jurusan">Jurusan</label>
+                            <input type="jurusan" name="jurusan" class="form-control" id="jurusan"
+                                aria-describedby="jurusan">
+                        </div>
+                        <div class="form-group">
+                            <label for="no_handphone">No_Handphone</label>
+                            <input type="no_handphone" name="no_handphone" class="form-control" id="no_handphone"
+                                aria-describedby="no_handphone">
+                        </div>
+                        <div class="form-group">
+                            <label for="photo">Photo</label>
+                            <input type="file" class="form-control-file" id="photo" name="photo">
+                        </div>
+                        <button type="submit" class="btn btn-primary">Submit</button>
+                    </form>
+                </div>
+            </div>
+        </div>
+    </div>
+@endsection
