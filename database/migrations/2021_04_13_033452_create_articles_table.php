9c521c887a243abf5444d2ac0c81fd74de9dd3b6
Syaifuddin Zuhri <mochammadsyaifuddinzuhri@gmail.com>
2021-04-13 10:37:51 +0700

    Edit Schema create_articles_table migration

--- database/migrations/2021_04_13_033452_create_articles_table.php
+++ database/migrations/2021_04_13_033452_create_articles_table.php
@@ -15,6 +15,9 @@ class CreateArticlesTable extends Migration
     {
         Schema::create('articles', function (Blueprint $table) {
             $table->id();
+            $table->string('title');
+            $table->text('content');
+            $table->string('feature_image');
             $table->timestamps();
         });
     }
