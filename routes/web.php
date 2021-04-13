a2620c6e540594448978f254b897fb2206cc3ad7
Syaifuddin Zuhri <mochammadsyaifuddinzuhri@gmail.com>
2021-04-13 14:53:54 +0700

    add route resource mahasiswa

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\ArticleController;
+use App\Http\Controllers\MahasiswaController;
 
 /*
 |--------------------------------------------------------------------------
@@ -20,3 +21,4 @@ Route::get('/', function () {
 
 Route::get('articles/print-pdf', [ArticleController::class, 'print_pdf'])->name('articles.print');
 Route::resource('articles', ArticleController::class);
+Route::resource('mahasiswa', MahasiswaController::class);
